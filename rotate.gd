3e8021ba9d55d0b3b1726e166b955510bb4a97e3
Scout <tuocs2001@gmail.com>
2021-08-13 15:52:35 -0700

    semifinal

--- rotate.gd
+++ rotate.gd
@@ -1,6 +1,9 @@
 extends Spatial
 
 export var rotSpeed = 100
+onready var timer = get_node("Timer1")
+onready var cam = get_node("Camera2")
+var rotate = false
 
 
 # Declare member variables here. Examples:
@@ -10,9 +13,17 @@ export var rotSpeed = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	timer.start(11.3)
+	timer.connect("timeout", self, "TimerTimeout")
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	rotate_y(rotSpeed * delta)
+	if (rotate):
+		rotate_y(rotSpeed * delta)
+
+
+func TimerTimeout():
+	rotate = true
+	cam.current = true
