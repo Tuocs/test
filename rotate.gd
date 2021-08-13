d7054639e691f052080f2e536aef8e1b345d07bd
Scout <tuocs2001@gmail.com>
2021-08-13 16:14:25 -0700

    good enough

--- rotate.gd
+++ rotate.gd
@@ -3,7 +3,9 @@ extends Spatial
 export var rotSpeed = 100
 onready var timer = get_node("Timer1")
 onready var cam = get_node("Camera2")
-var rotate = false
+var rotateS1 = false
+var rotateS2 = false
+var rotateS3 = false
 
 
 # Declare member variables here. Examples:
@@ -20,10 +22,21 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if (rotate):
+	if (rotateS1):
 		rotate_y(rotSpeed * delta)
+	if (rotateS2):
+		rotate_y(rotSpeed * 10 * delta)
 
 
 func TimerTimeout():
-	rotate = true
+	if (rotateS2):
+		rotateS3 = true
+		rotateS2 = false
+	if (rotateS1 && !rotateS2 && !rotateS3):
+		rotateS2 = true
+		timer.start(.4)
+	if (!rotateS1):
+		rotateS1 = true
+		timer.start(3)
 	cam.current = true
+
