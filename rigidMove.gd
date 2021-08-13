a5cdb6d370cc8151bde08b3191140556f1ba181f
Scout <tuocs2001@gmail.com>
2021-08-13 14:18:27 -0700

    remove fastfall

--- rigidMove.gd
+++ rigidMove.gd
@@ -14,8 +14,6 @@ func _process(delta):
 	var velocity = Vector2()
 	if Input.is_action_pressed("ui_up"):
 		velocity.y = -1
-	if Input.is_action_pressed("ui_down"):
-		velocity.y = 1
 	if Input.is_action_pressed("ui_left"):
 		velocity.x = -1
 	if Input.is_action_pressed("ui_right"):
