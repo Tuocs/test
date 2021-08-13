5d13ac65e78526235c730ac1561da4f2534b262f
Scout <tuocs2001@gmail.com>
2021-08-13 14:46:46 -0700

    scene switch

--- SceneSwitch.gd
+++ SceneSwitch.gd
@@ -13,4 +13,5 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_pressed("ui_cancel"):
+		get_tree().change_scene("res://new scene.tscn")
