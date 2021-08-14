8cb6993abd15df7ecae7c5250989606f87322a9f
Scout <tuocs2001@gmail.com>
2021-08-14 16:22:06 -0700

    organize stuff

--- Scripts/StaticShoot.gd
+++ Scripts/StaticShoot.gd
@@ -1,14 +1,15 @@
 extends Node2D
 
 
+
 # shoot if press esc
 func _process(delta):
-	if Input.is_action_just_pressed("ui_cancel"):
+	if Input.is_action_just_pressed("ui_page_up"):
 		shoot()
 
 #spawn bullet at location
 func shoot():
-	var Bullet = load ("res://Spawnables/Bullet.tscn")
+	var Bullet = load ("res://Scenes/Bullet.tscn")
 	var b = Bullet.instance()
 	add_child(b)
 	#b.transform = transform
