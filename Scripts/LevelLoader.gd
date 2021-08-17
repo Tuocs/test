30f376aafcd90b6acca10efcebaee9d58c2246bc
Scout <tuocs2001@gmail.com>
2021-08-17 03:11:38 -0700

    comments

--- Scripts/LevelLoader.gd
+++ Scripts/LevelLoader.gd
@@ -1,9 +1,12 @@
 extends Area2D
 
+#the target room number on room manger to go to
 export var targetRoom = 1
+
+#the side coming from or going to i havnt decided
 export var side = 0
 
 
 func _on_LevelLoader_body_entered(body):
-	if body.has_method("player_move"):
+	if body.has_method("player_move"):#shitty shitty way of doing this but it works, checks if player
 		get_tree().get_root().get_node("Level").ChangeRoom(targetRoom, side)
--- Scripts/LevelManager.gd
+++ Scripts/LevelManager.gd
@@ -1,31 +1,32 @@
 extends Node2D
 
+#types of rooms
 var Start = load ("res://Scenes/Rooms/TestLevel.tscn")
 var Second = load ("res://Scenes/Rooms/TestLevel - Copy.tscn")
 
+#list of rooms on floor
 var roomArray = [null, null, null, null, null, null, null, null, null]
+#current highest room
 var roomCount = 0
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	LoadRoom(Start)
-	LoadRoom(Second)
+	LoadRoom(Second)#this causes a problem with enemys tracking player before they go to room, should load later probably 
+						  #or just add the line of sight check before they more twords player, should do both
 	ChangeRoom(0, 0)
 
 
-#spawn in a room
+#spawn in a room into the world
 func LoadRoom(roomtype):
 	roomArray[roomCount] = roomtype.instance()
 	add_child(roomArray[roomCount])
-	roomArray[roomCount].position = Vector2(0, roomCount*1500)
 	move_child(roomArray[roomCount], 0)
+	roomArray[roomCount].global_position = Vector2(0, roomCount*1500)
 	roomCount += 1
 
 #move player to room
+#side was to show what side of room you entered from so you can spawn by the door
 func ChangeRoom(room, side):
-	get_node("PlayerScene/Player").position = Vector2(100, 100 + (room * 1500))
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	get_node("PlayerScene/Player").global_position = Vector2(200,(room * 1500)+400)
