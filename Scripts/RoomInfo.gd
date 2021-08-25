a027b594c9834b7ad107bfbb5d229793b3de630f
Scout <tuocs2001@gmail.com>
2021-08-25 15:53:27 -0700

    more of that

--- Scripts/RoomInfo.gd
+++ Scripts/RoomInfo.gd
@@ -4,26 +4,29 @@ extends Node2D
 var x = 0
 var y = 0
 var loaded = false
+var cleared = false
 
 export var doorMask = 0b0100
 
 
 func isLoaded():
 	return loaded
+func isCleared():
+	return cleared
 
 
 func setLoaded(loadState):
 	loaded = loadState
+	if cleared == false:
+		get_node("Enemies").spawnEnemies()
+func clearRoom():
+	cleared = true
 
 
 func getX():
 	return x
-
-
 func getY():
 	return y
-
-
 func setXY(_x, _y):
 	x = _x
 	y = _y
